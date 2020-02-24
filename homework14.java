122b6df3aacead0ba2f7bc6b609e3f014a0ba2de
17407111004 <1564062964@qq.com>
2020-02-24 20:41:11 +0800

    ListedList

---
+++ homework14.java
@@ -0,0 +1,47 @@
+package word2;
+
+public class homework14 {
+    //给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度
+    /*
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            if(s != null && s.length() > 0 && s != ""){
+                //String -> char[]
+                char[] strChar = s.toCharArray();
+                // 存储最长字串 key:char值，value:index下标
+                ArrayList<String> maxStr = new ArrayList<>();
+                //临时的字串存储空间
+                ArrayList<String> tempStr = new ArrayList<>();
+                //循环
+                for(int i=0; i<strChar.length; i++){
+                    //char -> String
+                    String str = new String(new char[]{strChar[i]});
+                    //判断str是否存在于tempStr中
+                    if(tempStr.contains(str)){
+                        //先判断tempStr的长度是否大于等于maxStr的长度,大于，才能将最长字串覆盖
+                        if(tempStr.size() > maxStr.size()){
+                            maxStr = new ArrayList<>(tempStr);
+                        }
+                        //存储重复字符
+                        int reIndex = tempStr.indexOf(str);
+                        // 删除tempStr中的重复字节及其之前的字符
+                        for(int j=0;j<=reIndex;j++){
+                            tempStr.remove(0);
+                        }
+                    }
+                    //将当前字符存入tempStr中
+                    tempStr.add(str);
+                }
+                //最终判断
+                if(tempStr.size() > maxStr.size()){
+                    maxStr = tempStr;
+                }
+                //返回最长字串的长度
+                return maxStr.size();
+            }
+            return 0;
+
+        }
+    }
+     */
+}
