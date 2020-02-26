c3b38c5853c7a80cba01f27ce2411a4bc6dd3fa9
17407111004 <1564062964@qq.com>
2020-02-26 21:50:12 +0800

    数组

---
+++ homework16.java
@@ -0,0 +1,34 @@
+package word5;
+
+public class homework16 {
+    /*
+    //在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+    public int findRepeatNumber(int[] nums) {
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[nums[i]]++;
+            if (arr[nums[i]] > 1) {
+                return nums[i];
+            }
+        }
+        return -1;
+
+    }
+     */
+//给定一个整数数组 nums，将该数组升序排列。
+    /*
+    public List<Integer> sortArray(int[] nums) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int i : nums) {
+            list.add(i);
+        }
+
+        Collections.sort(list);
+
+        return list;
+
+    }
+    
+     */
+}
