df11c95be00e3e51b18db37fe0c8c8f57e7221ac
17407111004 <1564062964@qq.com>
2020-02-28 21:54:42 +0800

    LinkedList

--- homework09.java
+++ homework09.java
@@ -1,9 +1,8 @@
-package word6;
-
-public class homework18 {
+package word7;
+public class homework09 {
     static class ListNode {
         int val;
-        LinkedList.ListNode next;
+        ListNode next;
 
         public ListNode(int val) {
             this.val = val;
@@ -11,17 +10,17 @@ public class homework18 {
     }
     //在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留
     // 返回链表头指针。 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
-    public LinkedList.ListNode deleteDuplication(LinkedList.ListNode pHead) {
-        LinkedList.ListNode newHead = new LinkedList.ListNode(-1);
-        LinkedList.ListNode newTail = newHead;
-        LinkedList.ListNode cur = pHead;
+    public ListNode deleteDuplication(ListNode pHead) {
+        ListNode newHead = new ListNode(-1);
+        ListNode newTail = newHead;
+        ListNode cur = pHead;
         while (cur != null) {
             if (cur.next != null && cur.val == cur.next.val) {
                 while (cur.next != null && cur.val == cur.next.val) {
                     cur = cur.next;
                 }
             } else {
-                newTail = new LinkedList.ListNode(cur.val);
+                newTail = new ListNode(cur.val);
                 newTail = newTail.next;
                 cur = cur.next;
             }
