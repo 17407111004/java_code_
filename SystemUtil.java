200130b64b435776bc7b55a1d1fa7e9b434f749f
17407111004 <1564062964@qq.com>
2020-03-21 21:05:24 +0800

    java_code

---
+++ SystemUtil.java
@@ -0,0 +1,12 @@
+package word13;
+
+public class SystemUtil {
+
+    public static boolean isAdmin(String userId){
+        return userId.toLowerCase()=="admin";
+    }
+    public static void main(String[] args){
+        System.out.println(isAdmin("Admin"));
+    }
+
+}
